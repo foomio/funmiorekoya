f5b49eb12ace2a63025ed44d79bc396262f0d46b
foomi <foomi.business@gmail.com>
2023-11-10 20:45:06 -0600

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-// App.js
 import React from 'react';
-import './App.css'; // Link to your App.css file
+import './App.css';
+import SoundClick from './SoundClick';
 
 function App() {
   return (
     <div className="app">
       <header>
-       
         <h1>Projects</h1>
         <p>Unreal Game Engine Showcase.</p>
       </header>
 
       <main>
-        <section>
-          <h2>Demos</h2>
-          
-        </section>
       
+          
+       
 
         <section>
-          <h2>Thank you!</h2>
-         
+          <h3>Project 1</h3>
+          {/* Content for Project 1 */}
+        </section>
+
+        <section>
+          <h3>Project 2</h3>
+          {/* Content for Project 2 */}
+        </section>
+
+        <section>
+          <h3>Project 3</h3>
+          {/* Content for Project 3 */}
+        </section>
+
+        <section>
+          <h3>Project 4</h3>
+          {/* Content for Project 4 */}
+        </section>
+
+        <section>
+          <h3>Project 5</h3>
+          {/* Content for Project 5 */}
+        </section>
+
+        <section>
+          <h3>Project 6</h3>
+          {/* Content for Project 6 */}
+        </section>
+
+        <section>
+          <h3>Project 7</h3>
+          {/* Content for Project 7 */}
+        </section>
+
+        <section>
+          <h3>Project 8</h3>
+          {/* Content for Project 8 */}
+        </section>
+
+        <section>
+          <h3>Project 9</h3>
+          {/* Content for Project 9 */}
         </section>
       </main>
 
